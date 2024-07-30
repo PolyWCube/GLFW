28b96b91cc5b8925178e78f93937f42c603e43e8
PolyWCube <polywcube@gmail.com>
2024-07-30 09:40:56 +0700

    Update essential file
    Delete unnessary file
    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,4 @@
 project "GLFW"
-	location "CustomEngine"
 	kind "StaticLib"
 	language "C"
 
@@ -7,11 +6,6 @@ project "GLFW"
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files {
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-	}
-	
-		includedirs {
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
 		"src/glfw_config.h",
@@ -24,7 +18,6 @@ project "GLFW"
 	}
 
 	filter "system:windows"
-		buildoptions { "-std=c11", "-lgdi32" }
 		systemversion "latest"
 		staticruntime "on"
 		
